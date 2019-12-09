8b1a14737a79b1fd40f7840d09fe0d8d596098a4
HolyPeople <holy.people@kakao.com>
2019-12-09 21:28:21 +0900

    ADD: Connection Dialog

--- Server/app.py
+++ Server/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/dialog')
 def hello_world():
-    return render_template("index.html")
+    data = request.json
+    if "apiKey" in data:
+        print(data["apiKey"])
+        if data["apiKey"] == "Hello, World":  # TODO: replace api key to jwt(javascript wep token)
+            if "message" in data:
+                return data['message']
+    return {"status": 404, "msg": "error"}
 
 
 if __name__ == '__main__':
