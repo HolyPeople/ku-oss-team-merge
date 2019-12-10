eca0e40196a0fa944c2e01e49127cd5807a46092
chahuigwang <chahuigwang@gmail.com>
2019-12-11 03:11:39 +0900

    nlg수정해야됨

---
+++ Server/Service/fine_dust_nlg.py
@@ -0,0 +1,142 @@
+
+#######################     TEST VARIABLES      ########################
+
+fineDust = 0;
+currentTime = 29494015
+currentDate = 2019-10-21
+currentYear = 2019
+currentMonth = 9
+currentDay = 29
+dateDifference = 0
+currentHour = 20    # test time. year month day hour minute
+currentMinute = 57
+timeDifference = 5      # 한 시간 단위
+
+#######################     TEST VARIABLES      ########################
+
+output = ""
+# return String value.
+
+def fineDustNLG( data ):
+
+    #data = {}        # JSON Decode to Dictionary
+
+
+    timeWord = "현재"
+    dateWord = "오늘"
+    year = ""
+    month = ""
+    day = ""
+    hour = ""
+    minute = ""
+    # second = ""
+    dateWordFlag = 0
+    avgTemperatureFlag = 1
+
+    currentDateFlag = 1
+    pastDateFlag = 0
+    futureDateFlag = 0
+    currentTimeFlag = 1
+    pastTimeFlag = 0
+    futureTimeFlag = 0
+
+    global output
+    # entityTime = {}
+    # entityDate = {}
+
+    if data["intent"] == "Date":
+        date = data["value"]
+        year = date.split('-', 3)[0]
+        month = date.split('-', 3)[1]
+        day = date.split('-', 3)[2]
+
+        output = output + year + "년 " + month + "월 " + day + "일   "
+
+        dateDifference = date - currentDate        # /***/ NEED CORRECTION !!!
+        #   positive => future, zero => now, negative => past
+
+        if dateDifference == 0:     # today
+            dateWordFlag = 1
+            output = output + date + "  "
+
+        elif dateDifference < 0:    # past
+            dateWordFlag = 1
+            currentDateFlag = 0
+            pastDateFlag = 1
+            output = output + date + "  "
+
+        elif dateDifference > 0:    # future
+            dateWordFlag = 1
+            currentDateFlag = 0
+            futureDateFlag = 1
+            output = output + date + "  "
+        else:
+            pass
+
+
+    if data["intent"] == "Time":
+        avgTemperatureFlag = 0
+        time = data["value"]
+        hour = time.split(':', 2)[0]
+        minute = time.split(':', 2)[0]
+        output = output + hour + "시 " + minute + "분  "
+
+        timeDifference = hour - currentHour    # NEED CORRECTION !!!    I only consider the hour-difference
+        #   positive => future time, zero => now, negative => past time
+
+        if dateDifference > 0:      # future
+            currentTimeFlag = 0
+            futureTimeFlag = 1
+            if hour <= 9 and hour > 6:  # 6~9시 아침
+                output = output + "아침 예상 기온은 "
+            elif hour >= 11 and hour <= 14:  # 11~14시 점심
+                output = output + "점심 예상 기온은 "
+            elif hour >= 17 and hour <= 20:  # 17~20시 저녁
+                output = output + "저녁 예상 기온은 "
+            elif hour > 20 and hour < 24:  # 20~24시 밤
+                output = output + "밤 예상 기온은 "
+            elif hour >= 0 and hour <= 6:  # 0~6시 새벽
+                output = output + "새벽 예상 기온은 "
+            else:
+                output = output + "예상 기온은 "  # default
+
+        elif dateDifference < 0:    # past
+            currentTimeFlag = 0
+            pastTimeFlag = 1
+            if hour <= 9 and hour > 6:              # 6~9시 아침
+                output = output + "아침 기온은 "
+            elif hour >= 11 and hour <= 14:         # 11~14시 점심
+                output = output + "점심 기온은 "
+            elif hour >= 17 and hour <= 20:         # 17~20시 저녁
+                output = output + "저녁 기온은 "
+            elif hour > 20 and hour < 24:           # 20~24시 밤
+                output = output + "밤 기온은 "
+            elif hour >= 0 and hour <= 6:           # 0~6시 새벽
+                output = output + "새벽 기온은 "
+            else:
+                output = output + "기온은 "          # default
+
+        else:                       # today !!
+            if abs(timeDifference) < 2:             # right now
+                currentTimeFlag = 1
+                output = output + "현재 기온은 "
+            else:
+                if hour <= 9 and hour > 6:  # 6~9시 아침
+                    output = output + "아침 기온은 "
+                elif hour >= 11 and hour <= 14:  # 11~14시 점심
+                    output = output + "점심 기온은 "
+                elif hour >= 17 and hour <= 20:  # 17~20시 저녁
+                    output = output + "저녁 기온은 "
+                elif hour > 20 and hour < 24:  # 20~24시 밤
+                    output = output + "밤 기온은 "
+                elif hour >= 0 and hour <= 6:  # 0~6시 새벽
+                    output = output + "새벽 기온은 "
+                else:
+                    output = output + "기온은 "  # default
+
+    if avgTemperatureFlag == 1:
+        output = output + str(avgTemperature) + "입니다."
+    else:
+        output = output + str(temperature) + "입니다."
+
+    return output
--- Server/Service/take_sensor_value.py
+++ Server/Service/take_sensor_value.py
@@ -1,51 +1,26 @@
 import requests
 import json
 
-
-def pm_dust():
+def fine_dust():
     url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty"
     data = {
         'ServiceKey': u'Bg9wG6ZtD1EtaCBxSTLokN2O8rZHfUhW+YQLayYHZjpxwOhyng3U/MkAR0vff8ynNiXT2v9o56hvIpyhI73Tgg==',
         'stationName': '광진구',
         'dataTerm': 'month',
-        'numOfRows': '5',
+        'numOfRows': '100',
         'ver': '1.3',
         '_returnType': 'json'
         }
-    return requests.get(url, params=data)
-    # url = "?stationName=%EA%B4%91%EC%A7%84%EA%B5%AC&dataTerm=month&pageNo=1&numOfRows=1&ServiceKey=%3D%3D&ver=1.3"
-    # request = ul.Request(url)
-    # response = ul.urlopen(request)
-    # rescode = response.getcode()
-    # if (rescode == 200):
-    #     responseData = response.read()
-    #     rD = xmltodict.parse(responseData)
-    #     rDJ = json.dumps(rD)
-    #     rDD = json.loads(rDJ)
-    #
-    #     w_data = rD["response"]["body"]["items"]["item"]
-    #
-    #     if (w_data["pm10Grade"] == '1'):
-    #         pm10Grade = '좋음'
-    #     elif (w_data["pm10Grade"] == '2'):
-    #         pm10Grade = '보통'
-    #     elif (w_data["pm10Grade"] == '3'):
-    #         pm10Grade = '나쁨'
-    #     elif (w_data["pm10Grade"] == '4'):
-    #         pm10Grade = '매우나쁨'
-    #     if (w_data["pm25Grade"] == '1'):
-    #         pm25Grade = '좋음'
-    #     elif (w_data["pm25Grade"] == '2'):
-    #         pm25Grade = '보통'
-    #     elif (w_data["pm25Grade"] == '3'):
-    #         pm25Grade = '나쁨'
-    #     elif (w_data["pm25Grade"] == '4'):
-    #         pm25Grade = '매우나쁨'
-    #
-    # return """측정 시각  : %s<br/>미세먼지(PM10) 농도 : %s<br/>미세먼지(PM2.5) 농도 : %s<br/>미세먼지(PM10) 농도 : %s<br/>미세먼지(PM2.5) 농도 :
-    #     %s\n" %(w_data["dataTime"], w_data["pm10Value"], w_data["pm25Value"], pm10Grade, pm25Grade) """
+    return json.loads(requests.get(url, params=data).text)['list']
 
+def particular_fine_dust(dataTime):
+    for i in range(1, 101):
+        if fine_dust()[i]['dataTime'] == dataTime:
+            return fine_dust()[i]['pm10Value']
+        else:
+            return fine_dust()[0]['pm10Value']
 
 if __name__ == "__main__" :
-    for li in json.loads(pm_dust().text)['list']:
+    for li in fine_dust():
         print(li)
+    # print(particular_fine_dust('2019-12-07 04:00'))
