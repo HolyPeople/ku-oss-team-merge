2f04e3ba330eef313f3eaa126d677886bdafe3e2
pythagorachu <51159308+pythagorachu@users.noreply.github.com>
2019-12-10 20:42:01 +0900

    First Edit of Humidity NLG. This verision is exactly equal to nlg(temerature) without the name of variables " 2019year 4month 20day today morning's temperature was 27. "

---
+++ Server/Service/nlg_humidity.py
@@ -0,0 +1,175 @@
+import json
+from . import sensor_data
+
+#######################     TEST VARIABLES      ########################
+
+humidity = 0.0       # current humidity
+avgHumidity = 1.0    # the average temp of the day
+currentTime = 29494015
+currentDate = 2019-10-21
+currentYear = 2019
+currentMonth = 9
+currentDay = 29
+dateDifference = 0
+currentHour = 20    # test time. year month day hour minute
+currentMinute = 57
+timeDifference = 5      # 한 시간 단위
+
+#######################     TEST VARIABLES      ########################
+
+output = ""
+# return String value.
+
+def humidityNLG( data ):
+
+    #data = {}        # JSON Decode to Dictionary
+
+
+    timeWord = "현재"
+    dateWord = "오늘"
+    year = ""
+    month = ""
+    day = ""
+    hour = ""
+    minute = ""
+    # second = ""
+    dateWordFlag = 0
+    avgHumidityFlag = 1
+
+    currentDateFlag = 1
+    pastDateFlag = 0
+    futureDateFlag = 0
+    currentTimeFlag = 1
+    pastTimeFlag = 0
+    futureTimeFlag = 0
+
+    global output
+    # entityTime = {}
+    # entityDate = {}
+
+    if data["intent"] == "Date":
+        date = data["value"]
+        year = date.split('-', 3)[0]
+        month = date.split('-', 3)[1]
+        day = date.split('-', 3)[2]
+
+        output = output + year + "년 " + month + "월 " + day + "일   "
+
+        dateDifference = date - currentDate        # /***/ NEED CORRECTION !!!
+        #   positive => future, zero => now, negative => past
+
+        if dateDifference == 0:     # today
+            dateWordFlag = 1
+            output = output + date + "  "
+
+        elif dateDifference < 0:    # past
+            dateWordFlag = 1
+            currentDateFlag = 0
+            pastDateFlag = 1
+            output = output + date + "  "
+
+        elif dateDifference > 0:    # future
+            dateWordFlag = 1
+            currentDateFlag = 0
+            futureDateFlag = 1
+            output = output + date + "  "
+        else:
+            pass
+
+
+    if data["intent"] == "Time":
+        avgHumidityFlag = 0
+        time = data["value"]
+        hour = time.split(':', 2)[0]
+        minute = time.split(':', 2)[0]
+        output = output + hour + "시 " + minute + "분  "
+
+        timeDifference = hour - currentHour    # NEED CORRECTION !!!    I only consider the hour-difference
+        #   positive => future time, zero => now, negative => past time
+
+        if dateDifference > 0:      # future
+            currentTimeFlag = 0
+            futureTimeFlag = 1
+            if hour <= 9 and hour > 6:  # 6~9시 아침
+                output = output + "아침 예상 습도는 "
+            elif hour >= 11 and hour <= 14:  # 11~14시 점심
+                output = output + "점심 예상 습도는 "
+            elif hour >= 17 and hour <= 20:  # 17~20시 저녁
+                output = output + "저녁 예상 습도는 "
+            elif hour > 20 and hour < 24:  # 20~24시 밤
+                output = output + "밤 예상 습도는 "
+            elif hour >= 0 and hour <= 6:  # 0~6시 새벽
+                output = output + "새벽 예상 습도는 "
+            else:
+                output = output + "예상 습도는 "  # default
+
+        elif dateDifference < 0:    # past
+            currentTimeFlag = 0
+            pastTimeFlag = 1
+            if hour <= 9 and hour > 6:              # 6~9시 아침
+                output = output + "아침 습도는 "
+            elif hour >= 11 and hour <= 14:         # 11~14시 점심
+                output = output + "점심 습도는 "
+            elif hour >= 17 and hour <= 20:         # 17~20시 저녁
+                output = output + "저녁 습도는 "
+            elif hour > 20 and hour < 24:           # 20~24시 밤
+                output = output + "밤 습도는 "
+            elif hour >= 0 and hour <= 6:           # 0~6시 새벽
+                output = output + "새벽 습도는 "
+            else:
+                output = output + "습도는 "          # default
+
+        else:                       # today !!
+            if abs(timeDifference) < 2:             # right now
+                currentTimeFlag = 1
+                output = output + "현재 습도는 "
+            else:
+                if hour <= 9 and hour > 6:  # 6~9시 아침
+                    output = output + "아침 습도는 "
+                elif hour >= 11 and hour <= 14:  # 11~14시 점심
+                    output = output + "점심 습도는 "
+                elif hour >= 17 and hour <= 20:  # 17~20시 저녁
+                    output = output + "저녁 습도는 "
+                elif hour > 20 and hour < 24:  # 20~24시 밤
+                    output = output + "밤 습도는 "
+                elif hour >= 0 and hour <= 6:  # 0~6시 새벽
+                    output = output + "새벽 습도는 "
+                else:
+                    output = output + "습도는 "  # default
+
+    if avgHumidityFlag == 1:
+        output = output + str(avgHumidity) + "입니다."
+    else:
+        output = output + str(humidity) + "입니다."
+
+    return output
+
+
+        # if time == "현재" or "아까":
+        #     if dateWordFlag == 0:
+        #         output = output + time
+        #     else:                       # 어제 현재 기온??? 오늘 현재 기온??? no no
+        #         pass
+        # elif time == "아침":
+        #     if currentTimeFlag == 1:    # if the date is today
+        #         if hour < 9:            # currentTime - date > 0 이면 과거, 오늘 아침이라고 하면?
+        #
+        #             pass
+        #         pass
+        #     output = output + time
+        # elif time == "점심":
+        #
+        #     pass
+        # elif time == "저녁":
+        #
+        #     pass
+        # elif time == "밤":
+        #
+        #     pass
+        # elif time == "새벽":
+        #
+        #     pass
+        # else:
+        #     hour = time.split(':', 2)[0]
+        #     minute = time.split(':', 2)[0]
+        #     output = output + hour + "시 " + "분 "
